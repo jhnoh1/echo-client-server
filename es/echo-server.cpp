04947bda57af4e1ef34b39c06b945bbedc1f7432
jhnoh1 <141809365+jhnoh1@users.noreply.github.com>
2024-05-13 01:31:10 +0900

    Update echo-server.cpp

--- es/echo-server.cpp
+++ es/echo-server.cpp
@@ -10,7 +10,7 @@
 void usage() {
 	printf("tcp server v1.0.0.5\n");
 	printf("\n");
-	printf("syntax: ts <port> [-e]\n");
+	printf("syntax: ts <port> [-e] [-b]\n");
 	printf("  -e : echo\n");
 	printf("sample: ts 1234\n");
 }
@@ -40,7 +40,7 @@ struct Param {
 
 std::vector<int> client_sockets;
 
-void recvThread(int sd) {
+void recvThread(int sd,bool echo, bool broadcast) {
 	printf("connected\n");
 	fflush(stdout);
 	static const int BUFSIZE = 65536;
@@ -49,26 +49,23 @@ void recvThread(int sd) {
 		ssize_t res = ::recv(sd, buf, BUFSIZE - 1, 0);
 		if (res == 0 || res == -1) {
 			fprintf(stderr, "recv return %ld", res);
-			myerror(" ");
 			break;
 		}
 		buf[res] = '\0';
 		printf("%s", buf);
 		fflush(stdout);
-		if (param.echo) {
+		if (echo) {
 			res = ::send(sd, buf, res, 0);
 			if (res == 0 || res == -1) {
 				fprintf(stderr, "send return %ld", res);
-				myerror(" ");
 				break;
 			}
 		}
-        if (param.broadcast) { 
+        if (broadcast) { 
             for (int client_sd : client_sockets) {
                 res = ::send(client_sd, buf, res, 0);
                 if (res == 0 || res == -1) {
                     fprintf(stderr, "broadcast send return %ld", res);
-                    myerror(" ");
                     break;
                 }
             }
@@ -80,67 +77,66 @@ void recvThread(int sd) {
 }
 
 int main(int argc, char* argv[]) {
-	if (!param.parse(argc, argv)) {
+	if (argc<2 || argc>4) {
 		usage();
 		return -1;
 	}
+	bool echo{false};
+    bool broadcast{false};
+	uint16_t port{0};
+	port = atoi(argv[1]);
+	for (int i = 1; i < argc;) {
+		if (strcmp(argv[i], "-e") == 0) {
+			echo = true;
+			i++;
+			continue;
+		}
+        if (strcmp(argv[i], "-b") == 0) { 
+            broadcast = true;
+            i++;
+            continue;
+        }
+	}
 
-	//
-	// socket
-	//
+
 	int sd = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (sd == -1) {
-		myerror("socket");
 		return -1;
 	}
-	//
-	// setsockopt
-	//
-	{
-		int optval = 1;
-		int res = ::setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-		if (res == -1) {
-			myerror("setsockopt");
-			return -1;
-		}
+
+	int optval = 1;
+	int res = ::setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+	if (res == -1) {
+		return -1;
 	}
-	//
-	// bind
-	//
+
 	{
 		struct sockaddr_in addr;
 		addr.sin_family = AF_INET;
-		addr.sin_addr.s_addr = param.srcIp;
-		addr.sin_port = htons(param.port);
+		addr.sin_port = htons(port);
 
 		ssize_t res = ::bind(sd, (struct sockaddr *)&addr, sizeof(addr));
 		if (res == -1) {
-			myerror("bind");
 			return -1;
 		}
 	}
 
-	//
-	// listen
-	//
 	{
 		int res = listen(sd, 5);
 		if (res == -1) {
-			myerror("listen");
 			return -1;
 		}
 	}
 
-	while (true) {
+	while (true) {	
 		struct sockaddr_in addr;
 		socklen_t len = sizeof(addr);
 		int newsd = ::accept(sd, (struct sockaddr *)&addr, &len);
 		if (newsd == -1) {
-			myerror("accept");
 			break;
 		}
 		client_sockets.push_back(newsd);
-		std::thread* t = new std::thread(recvThread, newsd);
+		std::thread* t = new std::thread(recvThread, newsd,echo,broadcast);
 		t->detach();
 	}
 	::close(sd);
