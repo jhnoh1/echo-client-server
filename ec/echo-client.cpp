17b264f97c9f8cae3167362f9c9f6ffe06b5234c
jhnoh1 <141809365+jhnoh1@users.noreply.github.com>
2024-05-12 21:27:43 +0900

    Update echo-client.cpp

--- ec/echo-client.cpp
+++ ec/echo-client.cpp
@@ -8,28 +8,6 @@
 #include <thread>
 
 
-void usage() {
-	printf("tcp client v1.0.0.5\n");
-	printf("\n");
-	printf("syntax: tc <ip> <port>\n");
-	printf("sample: tc 127.0.0.1 1234\n");
-}
-
-struct Param {
-	char* ip{nullptr};
-	char* port{nullptr};
-	uint32_t srcIp{0};
-	uint16_t srcPort{0};
-
-	bool parse(int argc, char* argv[]) {
-		for (int i = 1; i < argc;) {
-			ip = argv[1];
-			port =argv[2];
-		}
-		return (ip != nullptr) && (port != nullptr);
-	}
-} param;
-
 void recvThread(int sd) {
 	printf("connected\n");
 	fflush(stdout);
@@ -51,84 +29,40 @@ void recvThread(int sd) {
 	exit(0);
 }
 
-int main(int argc, char* argv[]) {
-	if (!param.parse(argc, argv)) {
-		usage();
+
+int main(int argc, char **argv){
+	if (argc !=3){
 		return -1;
 	}
+	int sd = ::socket(AF_INET, SOCK_STREAM, 0);
+	char* ip{nullptr};
+	char* port{nullptr};
+	ip = argv[1];
+	port =argv[2];
+	struct sockaddr_in server_add;
+	memset(&server_add,0,sizeof(server_add));
+	server_add.sin_family = AF_INET;
+	server_add.sin_port = htons((unsigned short)atoi(argv[2]));
+	server_add.sin_addr.s_addr=inet_addr(argv[1]);
 
-	//
-	// getaddrinfo
-	//
-	struct addrinfo aiInput, *aiOutput, *ai;
-	memset(&aiInput, 0, sizeof(aiInput));
-	aiInput.ai_family = AF_INET;
-	aiInput.ai_socktype = SOCK_STREAM;
-	aiInput.ai_flags = 0;
-	aiInput.ai_protocol = 0;
 
-	int res = getaddrinfo(param.ip, param.port, &aiInput, &aiOutput);
-	if (res != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(res));
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+	int optval = 1;
+	int setso = ::setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+	if (setso == -1) {
 		return -1;
 	}
-
-	//
-	// socket
-	//
-	int sd;
-	for (ai = aiOutput; ai != nullptr; ai = ai->ai_next) {
-		sd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
-		if (sd != -1) break;
-	}
-	if (ai == nullptr) {
-		fprintf(stderr, "cann not find socket for %s\n", param.ip);
+	int con = ::connect(sd, (struct sockaddr*)&server_add, sizeof(server_add));
+	if (con == -1) {
 		return -1;
 	}
-
-	//
-	// setsockopt
-	//
-	{
-		int optval = 1;
-		int res = ::setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-		if (res == -1) {
-			return -1;
-		}
-	}
-
-	//
-	// bind
-	//
-	if (param.srcIp != 0 || param.srcPort != 0) {
-		struct sockaddr_in addr;
-		addr.sin_family = AF_INET;
-		addr.sin_addr.s_addr = param.srcIp;
-		addr.sin_port = htons(param.srcPort);
-
-		ssize_t res = ::bind(sd, (struct sockaddr *)&addr, sizeof(addr));
-		if (res == -1) {
-			return -1;
-		}
-	}
-
-	//
-	// connect
-	//
-	{
-		int res = ::connect(sd, ai->ai_addr, ai->ai_addrlen);
-		if (res == -1) {
-			return -1;
-		}
-	}
-
 	std::thread t(recvThread, sd);
 	t.detach();
 
 	while (true) {
 		std::string s;
 		std::getline(std::cin, s);
-		s += "\r\n";
+		s += "\n";
 		ssize_t res = ::send(sd, s.data(), s.size(), 0);
 		if (res == 0 || res == -1) {
 			fprintf(stderr, "send return %ld", res);
@@ -136,4 +70,5 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	::close(sd);
+	
 }
